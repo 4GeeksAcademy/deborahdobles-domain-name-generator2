893139253f8feff505b4e09176a03d1b22134c41
Déborah Dobles <126917588+deborahdobles@users.noreply.github.com>
2023-04-15 02:30:53 +0000

    update domain name generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
+let pronouns = ["our", "his", "their", "my", "your"];
+let adjectives = ["great", "amazing", "fun", "outrageous", "bad"];
+let nouns = ["beach", "pool", "towel", "home", "family"];
+
+function generateDomainName(pronoun, adjective, noun) {
+  return `${pronoun}${adjective}${noun}.com`;
+}
+
+for (let pronoun of pronouns) {
+  for (let adjective of adjectives) {
+    for (let noun of nouns) {
+      console.log(`${pronoun}${adjective}${noun}.com`);
+    }
+  }
+}
